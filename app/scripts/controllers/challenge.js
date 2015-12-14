3c4bf624193b325d3951acad303837d91a5bbab7
Ward Debaene <ward.debaene.t4178@student.hogent.be>
2015-12-14 15:45:38 +0100

    challenges starten + layout challenges/home pagina

--- app/scripts/controllers/challenge.js
+++ app/scripts/controllers/challenge.js
@@ -3,6 +3,9 @@
 angular.module('EVA-Webapp-groep-17')
 .controller('ChallengeCtrl', ['ChallengeService', '$scope',
     function (ChallengeService, $scope) {
+    	$scope.setActive(challenge) = function(challenge){
+    		//challenge active zetten, FABRICE
+    	}
 
         ChallengeService.getAll().then(
         function (response) {
