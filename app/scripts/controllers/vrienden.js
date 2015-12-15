1dbdb33107b9479e6fdeb1659fc4576bad00f1a5
Tom De Backer <tom.debacker.u0758@student.hogent.be>
2015-12-15 13:52:35 +0100

    Layout vrienden en vrienden javascipt shizzle

--- app/scripts/controllers/vrienden.js
+++ app/scripts/controllers/vrienden.js
@@ -1,4 +1,4 @@
-﻿'use strict'
+'use strict'
 
 angular.module('EVA-Webapp-groep-17')
 .controller('VriendenCtrl', ['$scope', '$state', 'UserService',
@@ -7,6 +7,13 @@ angular.module('EVA-Webapp-groep-17')
         FB.api('/me/friends', { fields: 'name, picture' }, function (response) {
             $scope.$apply(function () {
                 $scope.vrienden = response.data;
+                $scope.vrienden.forEach(function(vriend) {
+                    UserService.getGebruikerByfacebookId(vriend.id).then(
+                        function (gebruiker) {
+                          vriend.aantalChallenges = gebruiker.data.data.gedaneChallenges.length;s
+                        }
+                    );
+                });
             })
         })
 
